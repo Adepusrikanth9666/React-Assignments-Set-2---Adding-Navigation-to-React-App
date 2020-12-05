eea462a5afa0de1a9bb61afd64ef2e177f9e5af5
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-12-05 16:09:44 +0530

    changes

--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -1,4 +1,12 @@
 import React from'react';
+import  useLocation from "react-router-dom";
+function usePageViews() {
+  let location = useLocation();
+  React.useEffect(() => {
+    console.log(location);
+  }, [location]);
+}
 export default function LocationDisplay(){
+  usePageViews();
  return <h1>You are home.</h1>
 }
