358122c9394b4496c871f3abc5dc26578e778607
Adepusrikanth9666 <64652479+Adepusrikanth9666@users.noreply.github.com>
2020-12-10 01:53:21 +0530

    changes

--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -2,12 +2,8 @@ import React from'react';
 // import  useLocation from "react-router-dom";
 import { useLocation } from 'react-router-dom'
 
-function HeaderView() {
-  const location = useLocation();
-  console.log(location.pathname);
-  return <div data-testid="location-display">{location.pathname}</div>
-}
 export default function LocationDisplay(){
-  return HeaderView();
-//  return <h1 data-testid="location-display">hello</h1>
+  const location = useLocation();
+  return <div data-testid="location-display">{location.pathname}</div>
+
 }
